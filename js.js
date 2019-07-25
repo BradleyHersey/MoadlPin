85a7a6bd8d55fd43b179f97b043d973d657a94ff
Brad <bradleyhersey00@gmail.com>
2019-07-25 15:29:44 -0400

    7/25/19

--- js.js
+++ js.js
@@ -1,48 +1,42 @@
 $(window).on("load", function() {
 // Get the modal
-var modal = document.getElementById("myModal");
 
+var modal = $("#myModal");
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+var btn = $("#myBtn");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var span = $(".close")[0];
 
 // When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
+$(btn).click(function(){
+  $(modal).css("display","block");
+});
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+$(span).click(function(){
+  $(modal).css("display","none")
+});
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+
+$(window).click(function(){
   if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-// password function 
-// function Wrong() {
-//     var x = document.getElementById("WrongPin");
-//     if (x.style.display === "none") {
-//       x.style.display = "block";
-//     } else {
-//       x.style.display = "none";
-//     }
-//   }
+    $(modal).css("display","none")
+       }
+});
   $("#SubBtn").click(function(){
       var Pin =$("#PW").val();
       var PinCode = '1234';
       switch(Pin === PinCode){ 
           case true:
               console.log("Worked");
-              modal.style.display = "none";
+              $(modal).css("display","none");
 
             break;
         case false:
+            $("#WrongPin").css("color", "red").css("display","block");
+           
+            
             console.log("also worked");
         }
      
